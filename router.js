190d355b264fb625a148e556420d6023df629906
Ron~Run <410142997@qq.com>
2020-04-23 23:03:54 +0800

    今天任务完成

--- router.js
+++ router.js
@@ -4,6 +4,7 @@ import home from './src/components/home.vue';
 import communicate from './src/components/communicate.vue';
 import message from './src/components/message.vue';
 import setting from './src/components/setting.vue';
+import news from './src/components/news.vue'
 
 Vue.use(VueRouter);
 const router = new VueRouter({
@@ -12,7 +13,8 @@ const router = new VueRouter({
         { path: '/home', component: home },
         { path: '/setting', component: setting },
         { path: '/communicate', component: communicate },
-        { path: '/message', component: message }
+        { path: '/message', component: message },
+        { path: '/home/news', component: news }
     ],
     linkActiveClass: "mui-active"
 });
